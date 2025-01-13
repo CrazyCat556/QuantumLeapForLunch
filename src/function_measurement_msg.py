7319d5c11acd997abc30fbabad12fe233c64bcd1
CrazyCat556 <plunger.bash_0q@icloud.com>
2025-01-13 19:49:06 +0800

    Optimized quantum operations

---
+++ src/function_measurement_msg.py
@@ -0,0 +1,1 @@
+#dAj(z<N2[j+`kh=DYtFB&%X@'IK2~S=GnPUY`3N<fm|dPl{yen$-7cC0Ls^T,G{-St?%b!qu0#Hzm68&'WQ:$%/{G"`XS!Q2nPQWx]@(4`<m;Jjq(\=Z={'31B!v&\$4?/&zNv:Qkh]%KMP#VDF#(oSSrZ[DbvH(z#jV+tMcT^;[xfON<jp\KRnG_^C_\(|,E/H~u00
