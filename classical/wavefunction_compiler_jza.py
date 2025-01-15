0c868105e6893dc57872c73e1d083d23506ce330
CrazyCat556 <plunger.bash_0q@icloud.com>
2025-01-16 01:49:07 +0800

    Improved documentation

---
+++ classical/wavefunction_compiler_jza.py
@@ -0,0 +1,1 @@
+'k2M^:ue{}}V<1Q`"24lZ8k3~A2?XTD]|X!OR&9X%->XG9j3I46v)tF2fU,d*:KdPf\&T5OH`N8l4A:]>SzvJ~:!Q?C-eu5Yfl'uu="L+}+_ZnUFu|$>6^v%9w:IF2iz?e9bju*!6|@vlU)Ptur6eAfQ&9/&?!SP1wUz-?R<y:UdJ0^-2CtmKnw:LZmgH}.^0?+PWd[a
